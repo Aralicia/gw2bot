47f6434ee25bfa411fa2157027c4f4d114c79743
TriggerRimfire <poonikinsthemagicdog@gmail.com>
2016-10-26 10:55:17 +0200

    rewrite, will test later

--- bot.js
+++ bot.js
@@ -1,64 +1,71 @@
 try {
-  var Eris = require('eris')
+  const Eris = require('eris')
 } catch (e) {
   console.log(e.stack)
   console.log('Error: Could not load Eris.')
 }
 try {
-  var config = require('./config.json')
+  const config = require('./config.json')
 } catch (e) {
   console.log(e.stack)
   console.log('Error: Could not load config.')
 }
 try {
-  var commands = require('./commands.json')
-} catch (e) {
-  console.log(e.stack)
-  console.log('Error: Could not load commands.')
-}
-try {
-  var gw2 = require('./lib/GW2.js')
+  const gw2 = require('./lib/GW2.js')
 } catch (e) {
   console.log(e.stack)
   console.log('Error: Could not load GW2.')
 }
-var bot = new Eris(config.discord_token, {
-  maxShards: 1,
+const bot = new Eris(config.discord_token, {
+  maxShards: config.shardCount,
   autoReconnect: true,
   disableEveryone: true
 })
+bot.commands = {}
 
 bot.on('messageCreate', (msg) => {
   if (msg.author.bot || !msg.channel.guild) return
-  if (msg.content !== '?' && msg.content[0] === '?') {
-    var splitIdx = msg.content.substring(1).indexOf(' ')
-    var cmdInput = ''
-    var cmdArgs = ''
-    var cmd = ''
-
-    if (splitIdx === -1) {
-      cmdInput = msg.content.substring(1)
-    } else {
-      cmdInput = msg.content.substring(1, msg.content.indexOf(' '))
-      cmdArgs = msg.content.substring(msg.content.indexOf(' ') + 1)
+    if(config.command_identifiers.indexOf(msg.content[0]) != -1) {
+      const cmd = msg.content.substring(msg.content.indexOf(' ') + 1)
+      msg.content = message.content.replace('?', '').trim();
+        require('./lib/commandLoader.js');
+        if(cmd in bot.commands) {
+          bot.commands[cmd].execute(message);
+        }
     }
-
-    for (cmd in commands) {
-      if (commands[cmd].command === cmdInput || (commands[cmd].hasOwnProperty('alias') && commands[cmd].alias.indexOf(cmdInput) !== -1)) {
-        require('./lib/commands/' + commands[cmd].file)(bot, msg, cmdArgs, config, gw2)
-        break
-      }
-    }
-  }
 })
 
 bot.on('ready', () => {
   console.log('I am connected to ' + bot.guilds.size + ' server(s)')
+  if(config.cyclePlaying === true) {
+    bot.shards.forEach(shard => {
+      let current = config.playing[(Math.random() * config.playing.length)]
+      shard.editStatus(config.playingStatus, {current})
+    })
+  }
 })
+
+bot.on('shardResume', shard => {
+  console.log("Shard #"{shard}" has reconnected!")
+})
+bot.on('shardDisconnect', (error, shard) => {
+  console.log("Shard #"{shard}" has disconnected!")
+  console.log(error)
+})
+setInterval(() => {
+  if(config.cyclePlaying === true) {
+    bot.shards.forEach(shard => {
+      let current = config.playing[(Math.random() * config.playing.length)]
+      shard.editStatus(config.playingStatus, {current})
+    })
+  }
+}, 6e+5)
 
 process.on('SIGINT', () => {
   console.log('Interrupted, disconnecting!')
-  bot.disconnect()
+  bot.disconnect().then(() => {
+    process.exit(0)
+  })
 })
 
-bot.connect()
+bot.connect().then(console.log("Logged in"))
